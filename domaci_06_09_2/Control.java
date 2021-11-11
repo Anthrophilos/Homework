81c36aa8e92410544712d6ef2a8fd6b0b276e097
Milan Panic <89742180+Anthrophilos@users.noreply.github.com>
2021-11-11 17:18:32 +0100

    Delete domaci_06_09_2 directory

--- domaci_06_09_2/Control.java
+++
@@ -1,9 +0,0 @@
-package domaci_06_09_2;
-
-
-	//Kreirati apstraktnu klasu Control koja ima:
-	 //apstraktnu metodu izvrsi akciju, koja kao parametar prima objekat VideoPlayer
-public abstract class Control {
-
-	public abstract void izvrsiAkciju(VideoPlayer vp);
-}
